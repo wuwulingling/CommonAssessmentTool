94f7b07d0c79f0c7384ec8da235e939b11d62d48
Keke Li <likeke@Kekes-MacBook-Air.local>
2024-11-23 23:51:37 -0800

    Add proper pytest test file

---
+++ tests/test_basic.py
@@ -0,0 +1,30 @@
+from logic import interpret_and_calculate
+from itertools import combinations_with_replacement
+from itertools import product
+
+# def test_interpret_and_calculate():
+#     print("running tests")
+#     data = {"23","1","1","1","1","0","1","2","2","3","2",
+#     "2","3","2","1","1","1","1","1","1","0","1","1","1"
+#     }
+#     result = interpret_and_calculate(data)
+#     print(data)
+
+
+
+# Cartesian product of [0, 1] repeated 2 times
+#result = list(product([0, 1], repeat=2))
+
+# Output: [(0, 0), (0, 1), (1, 0), (1, 1)]
+#print(result)
+
+#result = list(combinations_with_replacement([0, 1], 2))
+
+# Output: [(0, 0), (0, 1), (1, 1)]
+#print(result)
+
+def test_product():
+    """Test itertools product function"""
+    result = list(product([0, 1], repeat=2))
+    expected = [(0, 0), (0, 1), (1, 0), (1, 1)]
+    assert result == expected
